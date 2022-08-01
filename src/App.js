715d1d417d23444dec443de7439f5a17a67bf77c
Jenny C <jennyhc888@gmail.com>
2022-08-01 15:54:59 -0700

    add axios call to retrieve molecular formula

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+
+const kBaseUrl = "http://127.0.0.1:8000/lewis_structures_app";
+
+const getMolecules = async (e) => {
+  e.preventDefault();
+  try {
+    await axios.get(`${kBaseUrl}/api/`).then((response) => {
+      console.log(response);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <input type="submit" value="new-molecules" onClick={getMolecules}></input>
+    </section>
   );
 }
 
