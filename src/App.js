303b4aaf1823dbba1c98c739a76e87eb21687501
Adriana Gutierrez <jerez.adriana@gmail.com>
2022-08-11 19:54:49 -0400

    started adding instructions for game

--- src/App.js
+++ src/App.js
@@ -529,7 +529,14 @@ function App() {
         <Header />
         <div className="Left-comp">
           <MolecFormula display={molecFormula} />
-          <UserMessages message={message} />
+          <ul>
+            <li>Drag the atoms wherever you like within the stage.</li>
+            <li>Click two electrons to bond them.</li>
+            <li>Click a bond to delete it.</li>
+            <li>You can reset all bonds with the reset arrow on the stage.</li>
+          </ul>
+
+          {/* <UserMessages message={message} /> */}
           {/* <Buttons
             // updateMoleculeApp={updateMolecule}
             verifyStructureValidityApp={verifyStructureValidity}
